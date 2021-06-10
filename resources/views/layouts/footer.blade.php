a8b87ceb92fb84be3d13ddf761d15857fe7f04c7
dk009dk <kadavla999@gmail.com>
2021-06-10 16:04:29 +0530

    Contact Section

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,23 @@
     <footer class="footer relative pt-1 border-b-2 border-blue-700">
         <div class="container mx-auto px-6">
-            <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+            <div class="mt-5 border-t-2 border-gray-300 w-full flex flex-col items-center">
+                <div class="sm:w-2/3 text-center py-3">
+                    <p class="font-bold font-sans text-lg text-chemhunt-dark font-bold mb-2">
+                        For any query Contact
+                    </p>
+                    <div class="flex flex-col items-center w-full pt-2">
+                        <div class="text-chemhunt-logo">
+                            <div>
+                                <a href="mailto:chemhunt2.0iicheddu@gmail.com" target="_blank" >
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="sm:w-2/3 text-center py-6">
                     <p class="font-bold font-sans text-sm text-blue-700 font-bold mb-2">
                         © 2021 IIChE DDU SC
