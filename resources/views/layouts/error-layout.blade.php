8220dd7139664152c7fe18a00c80aeb069d6b35a
dk009dk <kadavla999@gmail.com>
2021-06-11 20:11:14 +0530

    Error Page Font

--- resources/views/layouts/error-layout.blade.php
+++ resources/views/layouts/error-layout.blade.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        <!-- Scripts -->
-        <script src="{{ asset('js/app.js') }}" defer></script>
-
-    </head>
-    <body class="font-caribbean bg-blue-300">
-        <div class="antialiased">
-            <div>
-                @include('layouts.chemhunt-logo')
-                {{ $slot }}
-            </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body class="font-sans font-bold bg-blue-300">
+<div class="antialiased">
+    <div class="antialiased">
+        <div>
+            @include('layouts.chemhunt-logo')
+            {{ $slot }}
+            @include('layouts.footer')
         </div>
-    </body>
+    </div>
+</div>
+</body>
 </html>
